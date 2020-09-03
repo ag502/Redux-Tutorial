68b53f3143d82d6571f8f09a088e76e88fb6fe07
ag502 <ag502@naver.com>
2020-09-04 01:22:51 +0900

    Define actions and reducer to add posts

---
+++ src/features/posts/postsSclice.js
@@ -0,0 +1,66 @@
+import {createSlice, nanoid} from '@reduxjs/toolkit'
+import {sub} from 'date-fns'
+
+const initialState = [
+    {
+        id: '1',
+        title: 'First Post',
+        content: 'Hello!',
+        user: '0',
+        data: sub(new Date(), {minutes: 10}.toISOString()),
+        reactions: {
+            thumbsUp: 0,
+            hooray: 0,
+            heart: 0,
+            rocket: 0,
+            eyes: 0
+        }
+    },
+    {
+        id: '2',
+        title: 'Second Post',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), {minutes: 5}.toISOString()),
+        reactions: {
+            thumbsUp: 0,
+            hooray: 0,
+            heart: 0,
+            rocket: 0,
+            eyes: 0
+        }
+    }
+]
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {
+        postAdded: {
+            reducer: (state, action) => {
+                state.push(action.payload)
+            },
+            prepare: (title, content, userId) => {
+                return {
+                    payload: {
+                        id: nanoid(),
+                        date: new Date().toISOString(),
+                        title,
+                        content,
+                        user: userId,
+                        reactions: {
+                            thumbsUp: 0,
+                            hooray: 0,
+                            heart: 0,
+                            rocket: 0,
+                            eyes: 0,
+                        }
+                    }
+                }
+            }
+        }
+    }
+})
+
+export const {postAdded} = postsSlice.actions
+export default postsSlice.reducer
